28a38745b3a420e3c1b62f5b40aa250fb611c5b6
karmveershubham <75744213+karmveershubham@users.noreply.github.com>
2020-12-21 09:04:02 +0530

    Add files via upload

---
+++ projectvote.py
@@ -0,0 +1,10 @@
+def main():
+    ageofperson = int(input('Enter your age '))
+    if ageofperson >= 18:
+        print('Your age is ', ageofperson, 'so you are eligible to caste your vote. Thanks!')
+    else:
+        print('Your age is', ageofperson, 'so you are too Young  to caste your Vote.')
+              
+if __name__=='__main__':
+    main()
+   
